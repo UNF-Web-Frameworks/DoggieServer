70d97308fca4d8ce73b88626a94303196ee8eb86
Jose C Gomez <me@josecgomez.dev>
2022-11-14 19:19:22 -0500

    Added Things.

--- src/models/dog.ts
+++ src/models/dog.ts
@@ -5,17 +5,19 @@ export class Dog {
   name: string;
   breed: string;
   age: number;
+  imageUrl:string='';
   createdBy:string='';
   updatedBy:string='';
   createdDate:Date;
   updatedDate:Date;
 
-  constructor(name: string, breed: string, age: number) {
+  constructor(name: string, breed: string, age: number,imageUrl:string='') {
     this.name = name;
     this.breed = breed;
     this.age = age;
     this.createdDate= new Date();
     this.updatedDate= new Date();
+    this.imageUrl=imageUrl;
   }
 
   
